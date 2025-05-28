527964f246475ba8b96bde22c523649975619110
gyan singh <gyansinghmatta@gmail.com>
2025-05-28 22:22:02 +0800

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,15 @@ class QuestionsController < ApplicationController
   def ask
 
   end
+def answer
+  @question = params[:question]
+
+  @answer = if @question.downcase.include?("i am going to work")
+        "Great!"
+elsif @question.include?("?")
+        "silly question, got dressed and go to work"
+else
+      "I dont care, get dressed and go to work "
+end
+end
 end
