5b0952ca2264b03fc92aa037d0ece16e732c055a
gyan singh <gyansinghmatta@gmail.com>
2025-05-28 22:34:15 +0800

    le wagon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  # Your code here, if any
+end
+class QuestionsController < ApplicationController
+  def ask
+    # This action just renders the ask form
+  end
+
+  def answer
+    @question = params[:question]
+
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
